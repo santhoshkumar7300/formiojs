29815f72986c3ab621f37caf93f1ebb8202552b7
Santhoshkumar <santhoshkumar.v@greenbotz.co>
2025-03-03 11:36:02 +0530

    Initial commit

--- components/html/HTML.js
+++ components/html/HTML.js
@@ -348,7 +348,7 @@ var HTMLComponent = /*#__PURE__*/ (function (_Component) {
         key: "builderInfo",
         get: function get() {
           return {
-            title: "HTML Element",
+            title: "HTML Element fsdfsdf",
             group: "layout",
             icon: "code",
             weight: 0,
