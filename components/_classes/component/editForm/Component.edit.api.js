29436f3988f070fcb94bcd8245c954bff47f1e74
Santhoshkumar <santhoshkumar.v@greenbotz.co>
2025-03-03 15:47:17 +0530

    modal api tab end point validation

--- components/_classes/component/editForm/Component.edit.api.js
+++ components/_classes/component/editForm/Component.edit.api.js
@@ -12,11 +12,11 @@ var _default = [
     input: true,
     key: "key",
     label: "Property Name",
-    tooltip: "The name of this field in the API endpoint.",
+    tooltip: "The name of this field in the API sss endpoint.",
     validate: {
       pattern: /^\w[\w.-]*\w?$/, // '(\\w|\\w[\\w-.]*\\w)',
       patternMessage:
-        "The property name must only contain alphanumeric characters, underscores, dots and dashes and should not be ended by dash or dot.",
+        "The property name must only contain alphanumeric sfsdfsd characters, underscores, dots and dashes and should not be ended by dash or dot.",
       required: true,
     },
   },
